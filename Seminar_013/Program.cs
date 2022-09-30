1afb223229e572b6f7afd8aff31830463ced76bb
TotskayaOV <olga.totskaya@gmail.com>
2022-10-01 02:19:01 +0700

    Created Seminat 012

---
+++ Seminar_013/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите порядковый номер дня недели: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+if (userNumber == 1)
+{
+    Console.WriteLine("1 -> Понедельник");
+}
+
+else if (userNumber == 2)
+{
+    Console.WriteLine("2 -> Вторник");
+}
+
+else if (userNumber == 3)
+{
+    Console.WriteLine("3 -> Среда");
+}
+
+else if (userNumber == 4)
+{
+    Console.WriteLine("4 -> Четверг");
+}
+
+else if (userNumber == 5)
+{
+    Console.WriteLine("5 -> Пятница");
+}
+
+else if (userNumber == 6)
+{
+    Console.WriteLine("6 -> Суббота");
+}
+
+else if (userNumber == 7)
+{
+    Console.WriteLine("7 -> Воскресенье");
+}
+
+else
+    Console.WriteLine("Введите порядковый номер дня земной недели");
