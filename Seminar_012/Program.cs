43eb040c3a747d5b6a1a5f0ec37d10a1e0218ff0
TotskayaOV <olga.totskaya@gmail.com>
2022-10-01 01:38:50 +0700

    Created Seminar 012

---
+++ Seminar_012/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumber2 = int.Parse(userInput2);
+
+if (userNumber2 * userNumber2 == userNumber1)
+{
+    Console.WriteLine("a=" + userNumber1 + ", b=" + userNumber2 + "-> ДА");
+}
+else 
+    Console.WriteLine("a=" + userNumber1 + ", b=" + userNumber2 + "-> НЕТ");
