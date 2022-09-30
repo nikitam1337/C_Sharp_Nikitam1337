e7b356847a3f07516a05eb7d917f336093c78c7b
TotskayaOV <olga.totskaya@gmail.com>
2022-10-01 00:50:12 +0700

    Created Seminar 011

---
+++ Seminar_011/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Напишите целое число: ");
+
+string userInput = Console.ReadLine() ?? "";
+
+int userNumber = int.Parse (userInput);
+
+Console.WriteLine (userNumber + " -> " + userNumber * userNumber);
+
