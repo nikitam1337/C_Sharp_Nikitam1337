27b9ba75f4caf92260588773f0f5a2a6ed730bde
TotskayaOV <olga.totskaya@gmail.com>
2022-10-01 13:47:52 +0700

    Created Exemple 005

---
+++ Exemple_005_HelloLikeUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "дима")
+{
+    Console.WriteLine("Ура, это же Дима!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);    
+}
