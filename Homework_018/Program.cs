723da2b490cb9387a2bd8926ebfb9f07f305feda
TotskayaOV <olga.totskaya@gmail.com>
2022-10-01 18:00:30 +0700

    Created Homework 018

---
+++ Homework_018/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int a = int.Parse(userInput);
+int x = 1;
+
+Console.Write(a + " -> ");
+
+while (x <= a)
+    if (x%2==0)
+    {
+        Console.Write(x);
+        if(a%2==0)
+        {
+            if(x < a)
+            {
+                Console.Write(", ");
+            }
+        }
+        else
+        {
+            if(x < a -1)
+            {
+                Console.Write(", ");
+            }
+        }
+        x += 1;
+    }
+    else
+    {
+        x +=1;
+    }
