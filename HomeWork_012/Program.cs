763ba6da31416a51f6bb114da61bbe575098d84a
TotskayaOV <olga.totskaya@gmail.com>
2022-10-01 15:55:19 +0700

    Created Homework 012

---
+++ HomeWork_012/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumber2 = int.Parse(userInput2);
+
+if (userNumber2 > userNumber1)
+{
+    Console.WriteLine(userNumber1 + " меньше чем " + userNumber2);
+}
+else 
+    Console.WriteLine(userNumber2 + " меньше чем " + userNumber1);
