f033a322c92f71a17d3d5d66c0ec94e500eef7f7
TotskayaOV <olga.totskaya@gmail.com>
2022-10-02 17:56:51 +0700

    Created Example 010, 011, 012

---
+++ Example_012_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿//создаем новый нулевой массив и рандомайзером заполняем его
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+//выводим в терминале элементы массива
+void PrintArray(int[] col)
+{
+    int count =col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //обозначение -1 вместо 0 вводится для обозачения случаев ненахождения find в массиве
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;  //останавливает исполнение цикла на первом же найденном find
+        }
+        index++;
+    }
+    return position;
+
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 40);
+Console.WriteLine(pos);
