78fabcfaf64a85dad60506a1f77c0a4e4595b8cf
TotskayaOV <olga.totskaya@gmail.com>
2022-10-05 02:04:57 +0700

    Added method to Seminar 022

--- Seminar_022/Program.cs
+++ Seminar_022/Program.cs
@@ -1,12 +1,29 @@
-﻿Console.Write("Введите первое число: ");
-string userInput1 = Console.ReadLine() ?? "";
-int a1 = int.Parse(userInput1);
+﻿int EnterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine($"Ошибка ввода!");
+    }
+    return number;
+}
 
-Console.Write("Введите второе число: ");
-string userInput2 = Console.ReadLine() ?? "";
-int a2 = int.Parse(userInput2);
+int a1 = EnterNumber();
+int a2 = EnterNumber();
 
-int a3 = a1%a2;
+//Console.Write("Введите первое число: ");
+//int a1 = int.Parse(Console.ReadLine() ?? "");
+//Console.Write("Введите второе число: ");
+//int a2 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Clear();
+Console.Write($"{a1}, {a2} -> ");
+
+
+int a3 = a1 % a2;
 if(a3==0)
     Console.WriteLine($"число {a1} кратно числу {a2}");
 else
