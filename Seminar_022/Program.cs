288eaf0a65a1ad8dbe5aee8aab25216e5005ec3f
TotskayaOV <olga.totskaya@gmail.com>
2022-10-05 01:29:30 +0700

    Created Seminar 022

---
+++ Seminar_022/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int a1 = int.Parse(userInput1);
+
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int a2 = int.Parse(userInput2);
+
+int a3 = a1%a2;
+if(a3==0)
+    Console.WriteLine($"число {a1} кратно числу {a2}");
+else
+    Console.WriteLine($"число {a1} не кратно числу {a2}, остаток от деления: {a3}");
