7c7808793b1915d77e1b5779f1dea7955ad4ffaf
TotskayaOV <olga.totskaya@gmail.com>
2022-10-05 01:14:27 +0700

    Created Seminar 021

---
+++ Seminar_021/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int a1 = num/100;
+int a2 = num%10;
+Console.WriteLine($"{a1}  {a2}");
+int a3 = a1*10;
+int a4 = a3 + a2;
+Console.WriteLine(a4);
