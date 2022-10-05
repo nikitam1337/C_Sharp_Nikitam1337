5b9419649c40889376eee9d9db6acfbf0a7d19fd
TotskayaOV <olga.totskaya@gmail.com>
2022-10-05 21:01:10 +0700

    Created Homework 215

---
+++ Homework_215/Program.cs
@@ -0,0 +1,21 @@
+﻿int EnterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+            break;
+        Console.WriteLine($"Ошибка ввода!");
+    }
+    return number;
+}
+
+int num1 = EnterNumber();
+
+if (5 < num1 && num1 < 8)
+    Console.WriteLine($"{num1} -> да");
+else if (num1 < 6)
+    Console.WriteLine($"{num1} -> нет");
+else if (7 < num1)
+    Console.WriteLine($"Ошибка ввода!");
