8e02818731fec00329b3d9ea706bd10c743d67db
TotskayaOV <olga.totskaya@gmail.com>
2022-10-08 01:28:20 +0700

    added comments Seminar 032

--- Seminar_032/Program.cs
+++ Seminar_032/Program.cs
@@ -1,24 +1,24 @@
-﻿
-int a1, a2, b1, b2;
+﻿int a1, a2, b1, b2;
 
 a1 = Koords("Введите координату х первого числа: ");
 a2 = Koords("Введите координату y первого числа: ");
 b1 = Koords("Введите координату х второго числа: ");
 b2 = Koords("Введите координату y второго числа: ");
-//Console.WriteLine(a1);
 
 double distance = Distance(a1, a2, b1, b2);
 Console.WriteLine(distance);
 
-static int Koords(string message)
+//а тут начались собсно методы:
+
+static int Koords(string message)   //Метод для ввода координат
 {
     Console.Write(message);
     int res = int.Parse(Console.ReadLine()); //ввод с консоли и присваивание этого числа res
-    return res;
+    return res;                     // возвращает значения координат
 }
 
-static double Distance(int a1, int a2, int b1, int b2)
+static double Distance(int a1, int a2, int b1, int b2)  //Метод для вычисления
 {
-    double res = Math.Sqrt(Math.Pow(b1 - a1, 2) + Math.Pow(b2 - a2, 2));
-    return res;
+    double res = Math.Sqrt(Math.Pow(b1 - a1, 2) + Math.Pow(b2 - a2, 2)); //Методы квадртаного корня и степени
+    return res;                                         // возвращает значение 
 }
