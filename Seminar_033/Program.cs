51972a13b07604fd605febc1a55322525224440e
TotskayaOV <olga.totskaya@gmail.com>
2022-10-08 01:43:27 +0700

    Created Seminar 033

---
+++ Seminar_033/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+
+PrintSquares(N);
+
+
+static void PrintSquares(int N)
+{
+    for(int i = 1; i < N; i++) //(переменная, до скольки работает цикл, условия изменения)
+    {
+        Console.Write($"{i*i}, ");
+    }
+    Console.Write($"{N*N}"); //Исключили запятую в конце вывода
+}
