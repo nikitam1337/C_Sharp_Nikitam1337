5ae0e09db8d9c4d61fcdc75c19a28b761b551c3c
TotskayaOV <olga.totskaya@gmail.com>
2022-10-08 20:45:26 +0700

    Created Homework 323

---
+++ Homework_323/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+
+PrintSquares(N);
+
+static void PrintSquares(int N)
+{
+    for(int i = 1; i < N; i++) 
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+    Console.Write($"{N * N * N}"); 
+}
