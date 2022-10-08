ec10e34d39675d2be6053f470016dbae2a64e09d
TotskayaOV <olga.totskaya@gmail.com>
2022-10-08 11:21:14 +0700

    Created Homework 319

---
+++ Homework_319/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+
+
+int a1 = N / 10000;
+int a2 = N / 1000 % 10;
+int a3 = N % 10;
+int a4 = N % 100 /10;
+
+if (a1 == a3)
+{
+    if (a2 == a4)
+        Console.Write("да");
+}
+else
+    Console.Write("нет");
